32b861f44ed66c9fc6be98374caa5bb4aee09c8a
Maciej Laskowski <mlaskowsk@gmail.com>
2017-08-13 13:15:51 +0200

    Init form with config values part 1

--- src/main/java/com/mlaskows/MainController.java
+++ src/main/java/com/mlaskows/MainController.java
@@ -5,16 +5,15 @@ import com.mlaskows.antsp.config.AcoConfigFactory;
 import com.mlaskows.antsp.datamodel.Solution;
 import com.mlaskows.antsp.datamodel.matrices.StaticMatrices;
 import com.mlaskows.antsp.datamodel.matrices.StaticMatricesBuilder;
+import com.mlaskows.antsp.solvers.AlgorithmType;
 import com.mlaskows.antsp.solvers.antsolvers.AntSystemSolver;
 import com.mlaskows.tsplib.datamodel.Tsp;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +32,10 @@ public class MainController implements Initializable {
 
     private Stage primaryStage;
 
+    private Tsp tsp;
+
+    // menu
+
     @FXML
     private MenuItem closeMenuItem;
 
@@ -44,6 +47,8 @@ public class MainController implements Initializable {
 
     @FXML
     private Canvas mapCanvas;
+
+    // info
 
     @FXML
     private Label nameLabel;
@@ -57,13 +62,32 @@ public class MainController implements Initializable {
     @FXML
     private Label solutionLenLabel;
 
+
+    // form
+
     @FXML
-    private ChoiceBox algorithmTypeChoiceBox;
+    private ChoiceBox<AlgorithmType> algorithmTypeChoiceBox;
+
+    @FXML
+    private TextField evaporationFactorTextField;
+
+    @FXML
+    private TextField pheromoneImportanceTextField;
+
+    @FXML
+    private TextField heuristicImportanceTextField;
+
+    @FXML
+    private TextField nnFactorTextField;
+
+    @FXML
+    private TextField antsCountTextField;
+
+    @FXML
+    private TextField maxStagnationTextField;
 
     @FXML
     private Button solveButton;
-
-    private Tsp tsp;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -71,7 +95,40 @@ public class MainController implements Initializable {
         openMenuItem.setOnAction(event -> openFile());
         solveMenuItem.setOnAction(event -> solve());
         solveButton.setOnAction(event -> solve());
-        algorithmTypeChoiceBox.setItems(FXCollections.observableArrayList("AS"));
+
+        algorithmTypeChoiceBox
+                .setItems(FXCollections.observableArrayList(AlgorithmType.values()));
+        algorithmTypeChoiceBox.getSelectionModel().selectFirst();
+        algorithmTypeChoiceBox.getSelectionModel()
+                .selectedItemProperty()
+                .addListener(
+                        (ObservableValue<? extends AlgorithmType> observable,
+                         AlgorithmType oldValue,
+                         AlgorithmType newValue) -> initFormForAlgoritmType(newValue)
+                );
+    }
+
+    private void initFormForAlgoritmType(AlgorithmType algorithmType) {
+        final AcoConfig config;
+        switch (algorithmType) {
+            case MIN_MAX:
+                config = AcoConfigFactory
+                        .createDefaultMinMaxConfig(tsp.getDimension());
+                break;
+            case RANK_BASED:
+                config = AcoConfigFactory
+                        .createDefaultRankedBasedConfig(tsp.getDimension());
+                break;
+            default:
+                config = AcoConfigFactory
+                        .createDefaultAntSystemConfig(tsp.getDimension());
+        }
+        evaporationFactorTextField.setText(String.valueOf(config.getPheromoneEvaporationFactor()));
+        pheromoneImportanceTextField.setText(String.valueOf(config.getPheromoneImportance()));
+        heuristicImportanceTextField.setText(String.valueOf(config.getHeuristicImportance()));
+        nnFactorTextField.setText(String.valueOf(config.getNearestNeighbourFactor()));
+        antsCountTextField.setText(String.valueOf(config.getAntsCount()));
+        maxStagnationTextField.setText(String.valueOf(config.getMaxStagnationCount()));
     }
 
     private void solve() {
@@ -95,14 +152,10 @@ public class MainController implements Initializable {
         nameLabel.setText(tsp.getName());
         dimensionLabel.setText(String.valueOf(tsp.getDimension()));
         commentLabel.setText(tsp.getComment());
+        initFormForAlgoritmType(algorithmTypeChoiceBox.getValue());
         new UnsolvedMapDrawer(mapCanvas, tsp).draw(ADDITIONAL_DRAW_SIZE);
     }
 
-    /**
-     * Sets primary stage.
-     *
-     * @param primaryStage
-     */
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
     }
